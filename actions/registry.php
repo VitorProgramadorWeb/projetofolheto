d50e3afc92ea4377a73cb3631631fa108858323b
VitorProgramadorWeb <vitor.programadorweb@gmail.com>
2024-03-06 11:56:17 -0300

    prepared statement php

--- actions/registry.php
+++ actions/registry.php
@@ -119,17 +119,21 @@ function setRegistry($table) {
             
             // Update (Edit)
             if ($id != "") {
-
-                $sql = "UPDATE users SET user = '$user', password = '$password', name = '$name', ".($birthdate == "" ? "" :  "birthdate = '$birthdate', ")."address = '$address', email = '$email', phone = '$phone', cpf = '$cpf' WHERE id = '$id'";
-                $db_response = $conn->query($sql);
+                
+                $stmt = $conn->prepare("UPDATE users SET user = ?, password = ?, name = ?, birthdate = ?, address = ?, email = ?, phone = ?, cpf = ? WHERE id = ?");
+                $stmt->bind_param("ssssssssi", $user, $password, $name, $birthdate, $address, $email, $phone, $cpf, $id);
+                $db_response_status = $stmt->execute();
 
                 $server_response = [
-                    "status"  => $db_response ? "edited": "failure",
-                    "message" => $db_response ? "Usuário editado com sucesso.": "Falha ao editar usuário."
+                    "status"  => $db_response_status ? "edited": "failure",
+                    "message" => $db_response_status ? "Usuário editado com sucesso.": "Falha ao editar usuário."
                 ];
-                if ($db_response) {
-                    $sql = "SELECT * FROM $table WHERE user = '$user'";
-                    $db_response = $conn->query($sql)->fetch_assoc();
+                if ($db_response_status) {
+                    $stmt->prepare("SELECT * FROM users WHERE id = ?");
+                    $stmt->bind_param("i", $id);
+                    $stmt->execute();
+
+                    $db_response = $stmt->get_result()->fetch_assoc();
 
                     // append user info:
                     $server_response["registry"] = [
@@ -145,32 +149,40 @@ function setRegistry($table) {
                     ];
                 }
 
+                $stmt->close();
                 return json_encode($server_response, JSON_PRETTY_PRINT);
 
             // Insert (Create)
             } else {
                 //Verifying if username already exists
-                $sql = "SELECT id, user FROM users WHERE user = '$user'"; // WHERE privilege = '$privilege'
-                $db_response = $conn->query($sql);
-                if ($db_response->num_rows > 0) {
+                $stmt = $conn->prepare("SELECT id, user FROM users WHERE user = ?"); // WHERE privilege = '$privilege'
+                $stmt->bind_param("s", $user);
+                $stmt->execute();
 
+                $db_response = $stmt->get_result();
+
+                if ($db_response->num_rows > 0) {
+                    $stmt->close();
                     return json_encode([
                         "status"  => "failure",
                         "message" => "Nome de usuário já existe."
                     ], JSON_PRETTY_PRINT);
 
                 } else {
-                    $sql = "INSERT INTO users(user, password, name,".($birthdate == "" ? "" : " birthdate,")." address, email, phone, cpf) " . 
-                    "values('$user', '$password', '$name',".($birthdate == "" ? "" : " 'birthdate',")." '$address', '$email', '$phone', '$cpf')";
-                    $db_response = $conn->query($sql);
+                    $stmt = $conn->prepare("INSERT INTO users(user, password, name, birthdate, address, email, phone, cpf) values(?, ?, ?, ?, ?, ?, ?, ?)"); // WHERE privilege = '$privilege'
+                    $stmt->bind_param("ssssssss", $user, $password, $name, $birthdate, $address, $email, $phone, $cpf);
+                    $db_response = $stmt->execute();
                     
                     $server_response = [
                         "status"  => $db_response ? "created": "failure",
                         "message" => $db_response ? "Usuário criado com sucesso.": "Falha ao criar usuário."
                     ];
                     if ($db_response) {
-                        $sql = "SELECT * FROM $table WHERE user = '$user'";
-                        $db_response = $conn->query($sql)->fetch_assoc();
+                        $stmt->prepare("SELECT * FROM users WHERE user = ?");
+                        $stmt->bind_param("s", $user);
+                        $stmt->execute();
+
+                        $db_response = $stmt->get_result()->fetch_assoc();
     
                         // append user info:
                         $server_response["registry"] = [
@@ -186,8 +198,151 @@ function setRegistry($table) {
                         ];
                     }
     
+                    $stmt->close();
                     return json_encode($server_response, JSON_PRETTY_PRINT);
                 }
+            }
+
+            break;
+
+        case "suppliers":
+            $id      = $_POST["id"];
+            $name    = $_POST["name"];
+            $address = $_POST["address"];
+            $email   = $_POST["email"];
+            $phone   = $_POST["phone"];
+            
+            // Update (Edit)
+            if ($id != "") {
+                
+                $stmt = $conn->prepare("UPDATE suppliers SET name = ?, address = ?, email = ?, phone = ? WHERE id = ?");
+                $stmt->bind_param("ssssi", $name, $address, $email, $phone, $id);
+                $db_response_status = $stmt->execute();
+
+                $server_response = [
+                    "status"  => $db_response_status ? "edited": "failure",
+                    "message" => $db_response_status ? "Fornecedor editado com sucesso.": "Falha ao editar fornecedor."
+                ];
+                if ($db_response_status) {
+                    $stmt->prepare("SELECT * FROM suppliers WHERE id = ?");
+                    $stmt->bind_param("i", $id);
+                    $stmt->execute();
+
+                    $db_response = $stmt->get_result()->fetch_assoc();
+
+                    // append supplier info:
+                    $server_response["registry"] = [
+                        "id"      => $db_response["id"],
+                        "name"    => $db_response["name"],
+                        "address" => $db_response["address"],
+                        "email"   => $db_response["email"],
+                        "phone"   => $db_response["phone"]
+                    ];
+                }
+
+                $stmt->close();
+                return json_encode($server_response, JSON_PRETTY_PRINT);
+
+            // Insert (Create)
+            } else {
+                $stmt = $conn->prepare("INSERT INTO suppliers(name, address, email, phone) values(?, ?, ?, ?)"); // WHERE privilege = '$privilege'
+                $stmt->bind_param("ssss", $name, $address, $email, $phone);
+                $db_response = $stmt->execute();
+                
+                $server_response = [
+                    "status"  => $db_response ? "created": "failure",
+                    "message" => $db_response ? "Fornecedor criado com sucesso.": "Falha ao criar fornecedor."
+                ];
+                if ($db_response) {
+                    $stmt->prepare("SELECT * FROM suppliers WHERE name = ? AND address = ? AND email = ? AND phone = ?");
+                    $stmt->bind_param("ssss", $name, $address, $email, $phone);
+                    $stmt->execute();
+
+                    $db_response = $stmt->get_result()->fetch_assoc();
+
+                    // append supplier info:
+                    $server_response["registry"] = [
+                        "id"      => $db_response["id"],
+                        "name"    => $db_response["name"],
+                        "address" => $db_response["address"],
+                        "email"   => $db_response["email"],
+                        "phone"   => $db_response["phone"]
+                    ];
+                }
+
+                $stmt->close();
+                return json_encode($server_response, JSON_PRETTY_PRINT);
+            }
+
+            break;
+
+        case "customers":
+            $id      = $_POST["id"];
+            $name    = $_POST["name"];
+            $address = $_POST["address"];
+            $email   = $_POST["email"];
+            $phone   = $_POST["phone"];
+            
+            // Update (Edit)
+            if ($id != "") {
+                
+                $stmt = $conn->prepare("UPDATE customers SET name = ?, address = ?, email = ?, phone = ? WHERE id = ?");
+                $stmt->bind_param("ssssi", $name, $address, $email, $phone, $id);
+                $db_response_status = $stmt->execute();
+
+                $server_response = [
+                    "status"  => $db_response_status ? "edited": "failure",
+                    "message" => $db_response_status ? "Cliente editado com sucesso.": "Falha ao editar cliente."
+                ];
+                if ($db_response_status) {
+                    $stmt->prepare("SELECT * FROM customers WHERE id = ?");
+                    $stmt->bind_param("i", $id);
+                    $stmt->execute();
+
+                    $db_response = $stmt->get_result()->fetch_assoc();
+
+                    // append customer info:
+                    $server_response["registry"] = [
+                        "id"      => $db_response["id"],
+                        "name"    => $db_response["name"],
+                        "address" => $db_response["address"],
+                        "email"   => $db_response["email"],
+                        "phone"   => $db_response["phone"]
+                    ];
+                }
+
+                $stmt->close();
+                return json_encode($server_response, JSON_PRETTY_PRINT);
+
+            // Insert (Create)
+            } else {
+                $stmt = $conn->prepare("INSERT INTO customers(name, address, email, phone) values(?, ?, ?, ?)"); // WHERE privilege = '$privilege'
+                $stmt->bind_param("ssss", $name, $address, $email, $phone);
+                $db_response = $stmt->execute();
+                
+                $server_response = [
+                    "status"  => $db_response ? "created": "failure",
+                    "message" => $db_response ? "Cliente criado com sucesso.": "Falha ao criar cliente."
+                ];
+                if ($db_response) {
+                    $stmt->prepare("SELECT * FROM customers WHERE name = ? AND address = ? AND email = ? AND phone = ?");
+                    $stmt->bind_param("ssss", $name, $address, $email, $phone);
+                    $stmt->execute();
+
+                    $db_response = $stmt->get_result()->fetch_assoc();
+
+                    // append customer info:
+                    $server_response["registry"] = [
+                        "id"      => $db_response["id"],
+                        "name"    => $db_response["name"],
+                        "address" => $db_response["address"],
+                        "email"   => $db_response["email"],
+                        "phone"   => $db_response["phone"]
+                    ];
+                }
+
+                $stmt->close();
+                return json_encode($server_response, JSON_PRETTY_PRINT);
             }
 
             break;
@@ -203,26 +358,77 @@ function setRegistry($table) {
     $conn->close();
 }
 
-function deleteRegistry($table, $primary_key) {
+function deleteRegistry($table, $id) {
     $conn = database_connection();
 
     switch ($table) {
         case "users":
-            $id = $primary_key;
+            $stmt = $conn->prepare("DELETE FROM users WHERE id=?"); // WHERE privilege = '$privilege'
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
 
-            $sql = "DELETE FROM users WHERE id='$id'";
-            $response = $conn->query($sql);
+            $db_response = $stmt->execute();
 
-            if ($response) {
+            if ($db_response) {
+                $stmt->close();
                 return json_encode([
                     "status"  => "deleted",
                     "message" => "Usuário excluido com sucesso."
                 ], JSON_PRETTY_PRINT);
 
             } else {
+                $stmt->close();
                 return json_encode([
                     "status"  => "failure",
                     "message" => "Não foi possível excluir o usuário."
+                ], JSON_PRETTY_PRINT);
+
+            }
+            break;
+
+        case "suppliers":
+            $stmt = $conn->prepare("DELETE FROM suppliers WHERE id=?"); // WHERE privilege = '$privilege'
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+
+            $db_response = $stmt->execute();
+
+            if ($db_response) {
+                $stmt->close();
+                return json_encode([
+                    "status"  => "deleted",
+                    "message" => "Fornecedor excluido com sucesso."
+                ], JSON_PRETTY_PRINT);
+
+            } else {
+                $stmt->close();
+                return json_encode([
+                    "status"  => "failure",
+                    "message" => "Não foi possível excluir o fornecedor."
+                ], JSON_PRETTY_PRINT);
+
+            }
+            break;
+
+        case "customers":
+            $stmt = $conn->prepare("DELETE FROM customers WHERE id=?"); // WHERE privilege = '$privilege'
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+
+            $db_response = $stmt->execute();
+
+            if ($db_response) {
+                $stmt->close();
+                return json_encode([
+                    "status"  => "deleted",
+                    "message" => "Cliente excluido com sucesso."
+                ], JSON_PRETTY_PRINT);
+
+            } else {
+                $stmt->close();
+                return json_encode([
+                    "status"  => "failure",
+                    "message" => "Não foi possível excluir o cliente."
                 ], JSON_PRETTY_PRINT);
 
             }
