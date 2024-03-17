98c556dba4d56d92e79d7c2d73008f0f30918f45
VitorProgramadorWeb <vitor.programadorweb@gmail.com>
2024-03-17 02:35:41 -0300

    Username and image in menu

--- patterns/_menu.php
+++ patterns/_menu.php
@@ -3,7 +3,7 @@
         <ul>
             <li class="menu"><a onclick="showMenu()" href="#menu"><img src="/projetointegrador/images/menu.svg" alt="Menu"></a></li>
             <li><a href="/projetointegrador/index.php">[LOGO]</a></li>
-            <li><a href="#">(0) User</a></li>
+            <li><a href="#"><img src="/projetointegrador/images/person.svg" alt="User image"><?php echo $_SESSION["username"]?></a></li>
         </ul>
     </nav>
 </header>
